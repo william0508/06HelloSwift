78624f99b1f822679e9aca48efec3eab0f0b88d7
徐文学 <william@xuwenxuedeMacBook-Pro.local>
2015-04-15 14:32:52 +0800

    Initial Commit

---
+++ 06HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  06HelloSwift
+//
+//  Created by 徐文学 on 15/4/15.
+//  Copyright (c) 2015年 william. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
